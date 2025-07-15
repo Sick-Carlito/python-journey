021d85c4655eb5ab23ad0a97a8b7ed34c580dc6e
Sick-Carlito <carlb1319@gmail.com>
2025-07-16 00:49:07 +0300

    Day 2 - variables and data types

---
+++ day02_variables/variables.py
@@ -0,0 +1,21 @@
+# Day 2 - Variables and Data Types
+
+# String
+my_name = "Carl"
+print("My name is", my_name)
+
+# Integer
+my_age = 25
+print("I am", my_age, "years old")
+
+# Float
+pi_value = 3.14159
+print("The value of pi is", pi_value)
+
+# Boolean
+is_learning = True
+print("Am I learning Python?", is_learning)
+
+# Play: Combine text + variables
+print("Hello, my name is " + my_name + ". I'm " + str(my_age) + " years old.")
+
