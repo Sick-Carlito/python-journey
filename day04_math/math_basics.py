6e9e304871a194275e6a9398aaa9ddb28b75c53e
Sick-Carlito <carlb1319@gmail.com>
2025-07-16 14:11:59 +0300

    Day 4 - Math and Calculator

---
+++ day04_math/math_basics.py
@@ -0,0 +1,24 @@
+# Day 4 - Python Math & Operators
+
+# Basic math
+a = 10
+b = 3
+
+
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a / b =", a / b)
+print("a // b =", a // b)
+print("a % b =", a % b)
+print("a ** b =", a ** b)
+
+# Order of operations
+result = 10 + 5 * 2
+print("10 + 5 * 2 =", result)  # Output: 20, not 30!
+
+# Grouping with parentheses
+result2 = (10 + 5) * 2
+print("(10 + 5) * 2 =", result2)
+
+
