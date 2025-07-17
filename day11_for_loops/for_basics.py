e04d72c2eda2f9da1c5ace35e700cc920cecd3e0
Sick-Carlito <carlb1319@gmail.com>
2025-07-17 12:55:54 +0300

    Day 11 - For Loops and To-Do App

---
+++ day11_for_loops/for_basics.py
@@ -0,0 +1,11 @@
+# Day 11 - For Loop Basics
+
+colors = ["red", "green", "blue"]
+
+for color in colors:
+    print("Color is:", color)
+
+
+for number in range(1, 11):  # 1 to 10
+    print(number * 2)
+
